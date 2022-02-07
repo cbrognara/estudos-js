6fba856059d857dd46945b5bd86bd9eb56b12483
Camila Brognara <cbrognara@gmail.com>
2022-02-07 16:06:53 -0300

    criacao do modal

---
+++ dt-money/src/App.tsx
@@ -0,0 +1,31 @@
+import { Dashboard } from './components/Dashboard'
+import { Header } from './components/Header'
+import { GlobalStyle } from './styles/global'
+import { useState } from 'react'
+import { NewTransactionModal } from './components/NewTransactionModal'
+
+function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
